3e48b81b24cddd384932ea07426e37ef1829034e
Michael <limichael909@gmail.com>
2022-06-08 18:53:01 -0400

    master

--- server/index.js
+++ server/index.js
@@ -21,18 +21,14 @@ client.connect().then(() => {
 var players={};
 app.use(cors());
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-
 //SOCKET IO SERVER
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "*",
-    credentials: "*"
+    credentials: true,
+    methods: ["PUT", "GET", "POST"]
   },
 });
 
