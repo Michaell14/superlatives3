cb8724f00919484ce87ba656400feaf5dca941f5
Michael <limichael909@gmail.com>
2022-06-07 22:58:58 -0400

    master

--- server/index.js
+++ server/index.js
@@ -26,8 +26,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
+    access: "*"
   },
 });
 
