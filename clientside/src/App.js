cf96ffc306bab7381b820b8e6e3ecec15e8213ff
Michael <limichael909@gmail.com>
2022-06-08 20:53:56 -0400

    master

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -29,7 +29,7 @@ for (let i=0; i<6; i++){
   roomC += keys[Math.floor(Math.random() * (keys.length-1) )];
 }
 
-var socket = io.connect("http://localhost:3001");
+var socket = io.connect("https://superlatives1.herokuapp.com/");
 
 //https://superlatives1.herokuapp.com/
 function App() {
